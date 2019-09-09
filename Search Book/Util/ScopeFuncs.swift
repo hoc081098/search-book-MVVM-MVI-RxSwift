b911d9e93b681f7d9bda113c9fc4e7904252e570
hoc081098 <hoc081098@gmail.com>
2019-09-09 21:07:56 +0700

    Add

---
+++ Search Book/Util/ScopeFuncs.swift
@@ -0,0 +1,23 @@
+//
+//  ScopeFuncs.swift
+//  Search Book
+//
+//  Created by HOANG TAN DUY on 9/9/19.
+//  Copyright © 2019 Petrus Nguyễn Thái Học. All rights reserved.
+//
+
+import Foundation
+
+protocol ScopeFunc { }
+
+extension ScopeFunc {
+    @inline(__always) func apply(block: (Self) -> ()) -> Self {
+        block(self)
+        return self
+    }
+    @inline(__always) func letIt<R>(block: (Self) -> R) -> R {
+        return block(self)
+    }
+}
+
+extension NSObject: ScopeFunc { }
